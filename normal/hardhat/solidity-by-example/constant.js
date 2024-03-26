8385bd62538160be415c661dcef77aa765e9a9e4
CGleowang <leo1105@codegreen.org>
2024-03-26 21:40:47 +0800

    feat: verifySignature

--- normal/hardhat/solidity-by-example/constant.js
+++ normal/hardhat/solidity-by-example/constant.js
@@ -1,6 +1,7 @@
 const contractsName = {
   FunctionSelector: "FunctionSelector",
   CarFactory: "CarFactory",
+  VerifySignature: "VerifySignature",
 };
 
 module.exports = { contractsName };
--- normal/hardhat/solidity-by-example/scripts/deploy.js
+++ normal/hardhat/solidity-by-example/scripts/deploy.js
@@ -2,22 +2,22 @@ const hre = require("hardhat");
 const { contractsName } = require("../constant");
 
 let signer, account1, account2;
-let contractName = contractsName.CarFactory;
+let contractName = contractsName.VerifySignature;
+let contractInstance;
 
 async function init() {
   // accounts = await ethers.getSigners();
   [signer, account1, account2] = await hre.ethers.getSigners();
   // account1 = accounts[1].address;
   // account2 = accounts[2].address;
-  const contractInstance = await hre.ethers.deployContract(contractName);
+  contractInstance = await hre.ethers.deployContract(contractName);
 
   await contractInstance.waitForDeployment();
 
   console.log(`deployed to ${contractInstance.target}`);
-  return contractInstance;
 }
 
-async function functionSelector(contractInstance) {
+async function functionSelector() {
   let selector = await contractInstance.getSelector(
     "transfer(address,uint256)"
   );
@@ -25,7 +25,7 @@ async function functionSelector(contractInstance) {
   //can be used with abi.encodeWithSelector()
 }
 
-async function createOtherContract(contractInstance) {
+async function createOtherContract() {
   await contractInstance.create(signer, "model1");
   console.log(await contractInstance.getCar(0));
   await contractInstance.create2AndSendEther(
@@ -39,8 +39,35 @@ async function createOtherContract(contractInstance) {
   console.log(await contractInstance.getCar(1));
 }
 
+/*
+signed msg off chain, check signature on chain
+*/
+async function verifySignature() {
+  const messageHash = await contractInstance.getMessageHash(
+    "0x14723a09acff6d2a60dcdf7aa4aff308fddc160c",
+    123,
+    "coffee and donuts",
+    1
+  );
+
+  // signed off chain
+  const signature = await signer.signMessage(hre.ethers.getBytes(messageHash));
+
+  const verified = await contractInstance.verify(
+    signer.address,
+    "0x14723A09ACff6D2A60DcdF7aA4AFf308FDDC160C",
+    123,
+    "coffee and donuts",
+    1,
+    signature
+  );
+
+  console.log("Signature verified:", verified);
+}
+
 (async () => {
-  let contractInstance = await init();
-  // await functionSelector(contractInstance);
-  await createOtherContract(contractInstance);
+  await init();
+  // await functionSelector();
+  // await createOtherContract();
+  await verifySignature();
 })();
