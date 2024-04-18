98b3fb427275d95fab7d8a46acfe4213c0aa9601
CGleowang <leo1105@codegreen.org>
2024-04-18 21:08:04 +0800

    feat: UniDirPayChannel

---
+++ normal/hardhat/solidity-by-example/test/UniDirectionalPaymentChannel.js
@@ -0,0 +1,3 @@
+//refer:
+// https://github.com/marcloeb/rareskills/blob/057d8d38e82c1e3806f2516563a44fc667f31f83/rsWeek16/_learningProjects/test/UniDirPayChan.t.sol
+// https://github.com/jacob-hanbeen-kim/project-s/blob/9f0e2c250e64902539a1d41d24c1d026557c0cd4/backend/contracts/UniDirectionalPaymentChannel.sol
