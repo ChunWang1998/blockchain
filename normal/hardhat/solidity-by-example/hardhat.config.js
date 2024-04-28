5eb0fdcbf23530c2724b2a529ba3e244a49df2a0
CGleowang <leo1105@codegreen.org>
2024-04-28 14:41:26 +0800

    chore: setting

--- normal/hardhat/solidity-by-example/hardhat.config.js
+++ normal/hardhat/solidity-by-example/hardhat.config.js
@@ -1,9 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("@openzeppelin/hardhat-upgrades");
+require("dotenv").config();
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
-  localhost: {
-    url: "http://127.0.0.1:8545",
+  defaultNetwork: "sepolia",
+  networks: {
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/` + process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.METAMASK_ACCOUNT_PRIVATE_KEY],
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/` + process.env.INFURA_API_KEY,
+      accounts: [process.env.METAMASK_ACCOUNT_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
   },
 };
